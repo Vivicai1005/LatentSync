aff93659c373ab55dfc538f4df336e9230b282c8
Vivicai1005 <caiweiwei1005@gmail.com>
2025-04-15 14:42:22 +0800

    edit inference steps

--- inference.sh
+++ inference.sh
@@ -3,7 +3,7 @@
 python -m scripts.inference \
     --unet_config_path "configs/unet/stage2.yaml" \
     --inference_ckpt_path "checkpoints/latentsync_unet.pt" \
-    --inference_steps 20 \
+    --inference_steps 50 \
     --guidance_scale 2.0 \
     --video_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/wanvideo/mi300_i2v_lady.mp4" \
     --audio_path "assets/demo1_audio.wav" \
