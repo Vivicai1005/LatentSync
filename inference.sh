cf8dd572918983dcf884bc7a84f26646cdc53a70
Vivicai1005 <caiweiwei1005@gmail.com>
2025-04-17 12:28:00 +0800

    edit video and audio path

--- inference.sh
+++ inference.sh
@@ -5,6 +5,6 @@ python -m scripts.inference \
     --inference_ckpt_path "checkpoints/latentsync_unet.pt" \
     --inference_steps 50 \
     --guidance_scale 2.0 \
-    --video_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/wanvideo/mi300_wan14_lady_4.mp4" \
-    --audio_path "assets/demo3_audio.wav" \
+    --video_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/mi300_wan14_lady_sec5.mp4" \
+    --audio_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/goku10_audio_10s.wav" \
     --video_out_path "video_out.mp4"
