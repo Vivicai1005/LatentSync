62896000f75c9fca2133698cf703e4f02146a211
Vivicai1005 <caiweiwei1005@gmail.com>
2025-04-23 10:33:12 +0800

    change audio file

--- inference.sh
+++ inference.sh
@@ -6,5 +6,5 @@ python -m scripts.inference \
     --inference_steps 50 \
     --guidance_scale 2.0 \
     --video_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/wanvideo/concatenated_lady.mp4" \
-    --audio_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/wanvideo/goku10_audio_10s.wav" \
+    --audio_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/wanvideo/goku9_audio_10s.wav" \
     --video_out_path "mi300_latentsync_concatenated_lady_audio9_10s.mp4"
