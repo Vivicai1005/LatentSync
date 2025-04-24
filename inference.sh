1c78ce6e381d831365b670676abc25d8f5a17550
Vivicai1005 <caiweiwei1005@gmail.com>
2025-04-24 16:03:07 +0800

    edit output name

--- inference.sh
+++ inference.sh
@@ -7,4 +7,4 @@ python -m scripts.inference \
     --guidance_scale 2.0 \
     --video_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/wanvideo/mi300_wan14_i2v_merged_lady2.mp4" \
     --audio_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/wanvideo/goku9_audio_10s.wav" \
-    --video_out_path "mi300_latentsync_concatenated_lady_audio9_10s.mp4"
+    --video_out_path "mi300_latentsync_lady2_audio9_10s.mp4"
