7b1ca084a1ca8e9e4b3b6b00022bc081ad4e1c1e
Vivicai1005 <caiweiwei1005@gmail.com>
2025-04-28 10:13:27 +0800

    edit path

--- inference.sh
+++ inference.sh
@@ -5,6 +5,6 @@ python -m scripts.inference \
     --inference_ckpt_path "checkpoints/latentsync_unet.pt" \
     --inference_steps 50 \
     --guidance_scale 2.0 \
-    --video_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/wanvideo/man1_merged_video.mp4" \
-    --audio_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/wanvideo/man1_audio.wav" \
+    --video_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/wanvideo/data/man1_merged_video.mp4" \
+    --audio_path "/home/weicai12/vivicai/DiffSynth-Studio/examples/wanvideo/data/man1_audio.wav" \
     --video_out_path "man1_audio1_8s.mp4"
